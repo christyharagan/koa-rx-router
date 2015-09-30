643d31af605b264be5d9a871904239060c74f58e
Christy Haragan <christy@christy.haragan@gmail.com>
2015-09-30 11:57:00 -0400

    Update to latest typescript typings

--- rxRouter.d.ts
+++ rxRouter.d.ts
@@ -1,29 +1,23 @@
-declare module "koa-rx-router" {
-  import {Observable} from 'rx'
+import {Observable} from 'rx'
 
-  module RxRouter {
-    export interface Options {
-      prefix: string
-    }
-  }
+export interface Options {
+  prefix: string
+}
 
-  class RxRouter {
-    constructor(opts?:RxRouter.Options)
+export class RxRouter {
+  constructor(opts?: Options)
 
-    get(name:string, path:string): Observable<any>
-    get(path:string): Observable<any>
+  get(name: string, path: string): Observable<any>
+  get(path: string): Observable<any>
 
-    post(name:string, path:string): Observable<any>
-    post(path:string): Observable<any>
+  post(name: string, path: string): Observable<any>
+  post(path: string): Observable<any>
 
-    put(name:string, path:string): Observable<any>
-    put(path:string): Observable<any>
+  put(name: string, path: string): Observable<any>
+  put(path: string): Observable<any>
 
-    del(name:string, path:string): Observable<any>
-    del(path:string): Observable<any>
+  del(name: string, path: string): Observable<any>
+  del(path: string): Observable<any>
 
-    routes(): any
-  }
-
-  export = RxRouter
+  routes(): any
 }
